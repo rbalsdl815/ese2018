da84d709aedf9a663549d87856a98b1a550a15d0
Gyumin Sim <rbalsdl815@naver.com>
2018-10-16 16:56:49 +0900

    added turbo on/off

--- myapp.c
+++ myapp.c
@@ -1,10 +1,25 @@
 #include "func.h"
 #include "func2.h"
 
+void turbo_on()
+{
+   printf("turbo on \n");
+}
+
+void turbo_off()
+{
+    printf("turbo off \n");
+}
+
 int main(void)
 {
+
+   turbo_on();
+
    foo1();
    foo2();
+
+   turbo_off();
 
  
 }
