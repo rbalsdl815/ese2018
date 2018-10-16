5d27ee5a929604d86ec693bd06a1a777760e529b
Gyumin Sim <rbalsdl815@naver.com>
2018-10-16 17:00:12 +0900

    added preprocessing

--- myapp.c
+++ myapp.c
@@ -1,8 +1,14 @@
 #include "func.h"
 #include "func2.h"
+#include<stdio.h>
 
+void preprocess()
+{
+    printf("do some preprocessing \n");
+}
 int main(void)
 {
+   preprocess();
    foo1();
    foo2();
 
